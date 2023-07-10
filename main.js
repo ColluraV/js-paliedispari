af2f0bd844513c4d4cd3ac307b79f78f5abc09de
ColluraV <valerio.18.02.89@hotmail.it>
2023-07-10 16:27:02 +0200

    aggiunta funzione pari o dispari

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const btnConferma = document.querySelector(".conferma");
 const risultati = document.querySelector("[id=risultato]");
 
 
-const pariODispari = document.querySelector("[id=pariDispari]");
+
 
 const pari= false;
 const dispari= false;
@@ -12,17 +12,42 @@ const dispari= false;
 
 btnConferma.addEventListener("click", function () {
 
-
+    const pariODispari = document.querySelector("[id=pariDispari]").value;
     const inputNum = document.querySelector("[id=numeroInput]").value;
-    console.log(inputNum)
+    console.log(inputNum);
+    console.log (pariODispari);
 
-//viene generato un numero rando m da 1 a 6 (da 1 a 5 dispari ha più piossibilità di vincere)
+//viene generato un numero random da 1 a 6 (da 1 a 5 dispari ha più piossibilità di vincere)
+
+    const numRandom = randomNumber()
+    console.log (numRandom)
 
 //sommiamo i due numeri
 
+    const somma = numRandom + parseInt(inputNum);
+    console.log ( somma );
+
+
 // verifichiamo se il risultato è pari o dispari
+
+  
 
 //dichiariamo il vinictore
 
 
 })
+
+function envenORnot(val){
+
+    if(val % 2 == 0 ){
+        envenORnot = pari;
+    } else {
+        envenORnot = dispari;
+    }
+    return envenORnot;
+}
+
+function randomNumber() {
+    const random = Math.floor(Math.random() * 6) + 1;
+    return random;
+}
