b6311c5c3cf3ae7784163b9b7e8a0e9dea1b776b
ColluraV <valerio.18.02.89@hotmail.it>
2023-07-10 17:01:52 +0200

    creata doppia cartella e iniziato il 2° es

---
+++ palindromo/main.js
@@ -0,0 +1,11 @@
+const btnConferma = document.querySelector(".conferma");
+const result = document.querySelector("[id=risultato]");
+
+//scriviamo il nome
+
+btnConferma.addEventListener("click", function () {
+
+
+
+    
+})
