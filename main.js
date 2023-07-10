54b5c9cec8cdb75685c4068d5537c7a030d86677
ColluraV <valerio.18.02.89@hotmail.it>
2023-07-10 15:51:33 +0200

    inserimento numero ok

---
+++ main.js
@@ -0,0 +1,28 @@
+//utente sceglie pari o dispari e il numero
+
+const btnConferma = document.querySelector(".conferma");
+const risultati = document.querySelector("[id=risultato]");
+
+
+const pariODispari = document.querySelector("[id=pariDispari]");
+
+const pari= false;
+const dispari= false;
+
+
+btnConferma.addEventListener("click", function () {
+
+
+    const inputNum = document.querySelector("[id=numeroInput]").value;
+    console.log(inputNum)
+
+//viene generato un numero rando m da 1 a 6 (da 1 a 5 dispari ha più piossibilità di vincere)
+
+//sommiamo i due numeri
+
+// verifichiamo se il risultato è pari o dispari
+
+//dichiariamo il vinictore
+
+
+})
